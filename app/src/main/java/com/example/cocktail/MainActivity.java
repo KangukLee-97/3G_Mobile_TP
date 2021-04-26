6a73ea6e1897f4527396f058c313c22106528c40
tnfpck0306 <76057758+tnfpck0306@users.noreply.github.com>
2021-04-26 13:37:25 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/cocktail/MainActivity.java
+++ app/src/main/java/com/example/cocktail/MainActivity.java
@@ -20,72 +20,25 @@ public class MainActivity extends AppCompatActivity {
             startLoginActivity();
         }
 
-//         findViewById(R.id.btnLogout).setOnClickListener(onClickListener);
+        findViewById(R.id.btnLogout).setOnClickListener(onClickListener);
         
-        ListView listView = findViewById(R.id.listView);
-        CocktailAdapter adapter = new CocktailAdapter();
-        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
-        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
-        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
-        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
-        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
-        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
-        listView.setAdapter(adapter);
     }
     
-    class CocktailAdapter extends BaseAdapter {
-        ArrayList<CocktailRecipe> recipes = new ArrayList<CocktailRecipe>();
-
+  
+    View.OnClickListener onClickListener=new View.OnClickListener() {
         @Override
-        public int getCount() {
-            return recipes.size();
-        }
-
-        public void addItem(CocktailRecipe recipe){
-            recipes.add(recipe);
-        }
-
-        @Override
-        public Object getItem(int position) {
-            return recipes.get(position);
-        }
-
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            CocktailRecipeView cocktailRecipeView = null;
-
-            if(convertView == null) {
-                cocktailRecipeView = new CocktailRecipeView(getApplicationContext());
-            } else {
-                cocktailRecipeView = (CocktailRecipeView)convertView;
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.btnLogout:
+                    FirebaseAuth.getInstance().signOut();
+                    startLoginActivity();
+                    break;
             }
-            CocktailRecipe recipe = recipes.get(position);
-            cocktailRecipeView.setName(recipe.getName());
-            cocktailRecipeView.setTag(recipe.getTag());
-            cocktailRecipeView.setImage(recipe.getResId());
-            return cocktailRecipeView;
         }
+    };
+
+    private void startLoginActivity() {
+        Intent intent=new Intent(this, loginActivity.class);
+        startActivity(intent);
     }
-
-//     View.OnClickListener onClickListener=new View.OnClickListener() {
-//         @Override
-//         public void onClick(View v) {
-//             switch(v.getId()){
-//                 case R.id.btnLogout:
-//                     FirebaseAuth.getInstance().signOut();
-//                     startLoginActivity();
-//                     break;
-//             }
-//         }
-//     };
-
-//     private void startLoginActivity() {
-//         Intent intent=new Intent(this, loginActivity.class);
-//         startActivity(intent);
-//     }
 }
