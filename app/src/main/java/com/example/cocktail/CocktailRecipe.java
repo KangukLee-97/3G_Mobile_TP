1b7e83c6b667ec91627216c4f9068f5c9c354caf
tnfpck0306 <76057758+tnfpck0306@users.noreply.github.com>
2021-04-26 13:14:29 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/cocktail/CocktailRecipe.java
@@ -0,0 +1,41 @@
+package gachon.example.hello;
+
+public class CocktailRecipe {
+    String name;
+    String tag;
+    int resId;
+
+    public CocktailRecipe(String name, String tag, int resId) {
+        this.name = name;
+        this.tag = tag;
+        this.resId = resId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public int getResId() {
+        return resId;
+    }
+
+    @Override
+    public String toString() {
+        return "CocktailRecipe{" +
+                "name='" + name + '\'' +
+                ", mobile='" + tag + '\'' +
+                '}' ;
+    }
+}
