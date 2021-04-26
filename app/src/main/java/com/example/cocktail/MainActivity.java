092151ec19404acf37670b1542b2fd900e1857e6
tnfpck0306 <76057758+tnfpck0306@users.noreply.github.com>
2021-04-26 13:51:27 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/cocktail/MainActivity.java
+++ app/src/main/java/com/example/cocktail/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +28,55 @@ public class MainActivity extends AppCompatActivity {
 
         findViewById(R.id.btnLogout).setOnClickListener(onClickListener);
         
+        ListView listView = findViewById(R.id.listView);
+        CocktailAdapter adapter = new CocktailAdapter();
+        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
+        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
+        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
+        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
+        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
+        adapter.addItem(new CocktailRecipe("Cocktail name", "tag", R.drawable.design));
+        listView.setAdapter(adapter);
+        
+    }
+    
+    class CocktailAdapter extends BaseAdapter {
+        ArrayList<CocktailRecipe> recipes = new ArrayList<CocktailRecipe>();
+
+        @Override
+        public int getCount() {
+            return recipes.size();
+        }
+
+        public void addItem(CocktailRecipe recipe){
+            recipes.add(recipe);
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return recipes.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            CocktailRecipeView cocktailRecipeView = null;
+
+            if(convertView == null) {
+                cocktailRecipeView = new CocktailRecipeView(getApplicationContext());
+            } else {
+                cocktailRecipeView = (CocktailRecipeView)convertView;
+            }
+            CocktailRecipe recipe = recipes.get(position);
+            cocktailRecipeView.setName(recipe.getName());
+            cocktailRecipeView.setTag(recipe.getTag());
+            cocktailRecipeView.setImage(recipe.getResId());
+            return cocktailRecipeView;
+        }
     }
     
   
