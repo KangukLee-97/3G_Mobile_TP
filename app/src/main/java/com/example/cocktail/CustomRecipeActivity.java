dee80af5ebb59782a3f49311959491b2c4c43559
chkang <chkangsc@gmail.com>
2021-05-26 23:20:26 +0900

    디자인

--- app/src/main/java/com/example/cocktail/CustomRecipeActivity.java
+++ app/src/main/java/com/example/cocktail/CustomRecipeActivity.java
@@ -6,12 +6,20 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cocktail.Fragment.CustomFragment;
@@ -19,6 +27,7 @@ import com.example.cocktail.View.AddInfo;
 import com.example.cocktail.adapter.RecipeAdpater;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -36,10 +45,51 @@ public class CustomRecipeActivity extends AppCompatActivity {
     private String image;
     private String title;
     private static final String TAG="CustomRecipe";
+    private DrawerLayout drawerLayout;
+    private View drawerView;
+    private ListView listView;
+    private String [] menu = {"레시피","커스텀 레시피","마이페이지","주조기능사","로그아웃"};
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customrecipe);
+
+        //맨 위에 툴바 적용
+        Toolbar toolbar;
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar=getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //navigation menu
+        drawerLayout=(DrawerLayout)findViewById(R.id.drawerRecipe);
+        drawerView=(View)findViewById(R.id.drawer);
+        listView = (ListView) findViewById (R.id. list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1 ,menu);
+        listView.setAdapter (adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (menu[position]){
+                    case "레시피":
+                        startRecipeActivity();
+                        break;
+                    case "마이페이지":
+                        startUserInfoActivity();
+                        break;
+                    case "커스텀 레시피":
+                        startCustomActivity();
+                        break;
+                    case "주조기능사":
+                        startTestActivity();
+                        break;
+                    case "로그아웃":
+                        FirebaseAuth.getInstance().signOut();
+                        startLoginActivity();
+                        break;
+                }
+            }
+        });
 
         Intent intent=getIntent();
         title=intent.getStringExtra("Title");
@@ -74,5 +124,46 @@ public class CustomRecipeActivity extends AppCompatActivity {
             e.getMessage();
             return null;
         }
+    }
+
+    //툴바 왼쪽에 아이콘 클릭시 navigation menu 열리도록 만듦
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(drawerView);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //로그인 activity로 이동
+    private void startLoginActivity() {
+        Intent intent=new Intent(this, loginActivity.class);
+        startActivity(intent);
+    }
+
+    //recipe activity로 이동
+    private void startRecipeActivity() {
+        Intent intent=new Intent(this, RecipeActivity.class);
+        startActivity(intent);
+    }
+
+    //userInfo activity로 이동
+    private void startUserInfoActivity() {
+        Intent intent=new Intent(this, UserInfoActivity.class);
+        startActivity(intent);
+    }
+
+    //custom recipe activity로 이동
+    private void startCustomActivity() {
+        Intent intent=new Intent(this, CustomActivity.class);
+        startActivity(intent);
+    }
+
+    // test activity로 이동
+    private void startTestActivity() {
+        Intent intent = new Intent(this, testActivity.class);
+        startActivity(intent);
     }
 }
