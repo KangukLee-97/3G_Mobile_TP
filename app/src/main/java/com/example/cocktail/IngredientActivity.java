07c37463cdad1f57cba6096113b4baf4da9ba827
Kanguk Lee <rkddnrdl97@naver.com>
2021-05-30 18:59:44 +0900

    Ingredient

--- app/src/main/java/com/example/cocktail/IngredientActivity.java
+++ app/src/main/java/com/example/cocktail/IngredientActivity.java
@@ -1,7 +1,13 @@
 package com.example.cocktail;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
@@ -19,8 +25,11 @@ public class IngredientActivity extends AppCompatActivity {
 
     private FirebaseFirestore db;
     private ArrayList<String> ingredientList;
+    private ArrayList<String> selectedIngredList;
+    private ArrayAdapter<String> adapter;
 
     private ListView ingred_listview;
+    private Button ingred_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +37,9 @@ public class IngredientActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ingredient);
 
         ingredientList = new ArrayList<String>();
+        selectedIngredList = new ArrayList<String>();
         ingred_listview = (ListView)findViewById(R.id.ingredient_listview);
-
+        ingred_button = (Button)findViewById(R.id.ingredient_submit_btn);
 
         // Firestore에서 ingredient 참조 후 가져오기
         db = FirebaseFirestore.getInstance();
@@ -41,14 +51,27 @@ public class IngredientActivity extends AppCompatActivity {
                         if(task.isSuccessful()){
                             for(QueryDocumentSnapshot document : task.getResult()) {
                                 String ingredient = document.getData().get("Name").toString();
-                                ingredientList.add(ingredient);
+                                ingredientList.add(ingredient);   // 전체 재료 List에 추가
                             }
-                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_multiple_choice, ingredientList);
+
+                            // ListView에 전체 재료들 Upload
+                            adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_multiple_choice, ingredientList);
                             ingred_listview.setAdapter(adapter);
+
+                            // 선택하기 버튼
+                            ingred_button.setOnClickListener(new View.OnClickListener(){
+                                @Override
+                                public void onClick(View v){
+                                    SparseBooleanArray checkedItems = ingred_listview.getCheckedItemPositions();
+
+                                    for(int i=0; i<ingredientList.size(); i++) {
+                                        if(checkedItems.get(i) == true)
+                                            selectedIngredList.add(ingredientList.get(i));   // 선택된 재료들 list
+                                    }
+                                }
+                            });
                         }
                     }
                 });
-
-
     }
 }
