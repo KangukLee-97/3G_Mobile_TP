bf5ebe14d2b0c12eece72497a15dbee733345077
KangukLee-97 <rkddnrdl97@naver.com>
2021-05-31 21:26:51 +0900

    Final

--- app/src/main/java/com/example/cocktail/IngredientActivity.java
+++ app/src/main/java/com/example/cocktail/IngredientActivity.java
@@ -86,24 +86,6 @@ public class IngredientActivity extends AppCompatActivity {
                             adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_multiple_choice, ingredientList);
                             ingred_listview.setAdapter(adapter);
 
-                            ingredSearch.addTextChangedListener(new TextWatcher() {
-                                @Override
-                                public void afterTextChanged(Editable edit) {
-                                    String filterText = edit.toString();
-
-                                    if (filterText.length() > 0)
-                                        ingred_listview.setFilterText(filterText);
-                                    else
-                                        ingred_listview.clearTextFilter();
-                                }
-
-                                @Override
-                                public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-                                @Override
-                                public void onTextChanged(CharSequence s, int start, int before, int count) {}
-                            });
-
                             // 선택하기 버튼
                             ingred_button.setOnClickListener(new View.OnClickListener(){
                                 @Override
